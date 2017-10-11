66f27104cee23c69f79f15ff512fef73c08f1b94
Anne-Laure Bienaimé <annelaure.bienaime@gmail.com>
2017-10-11 14:56:35 +0200

    Work menu

--- js/app.js
+++ js/app.js
@@ -4,6 +4,22 @@ $('#burger_icon, .close').on('click',function(){
 }); 
 
 // Effet barre défilante
-$(document).ready(function(){
-  $('#navbar_social').slideDown();
+$(function() {
+    var header = $("#navbar_social");
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+        if (scroll >= 200) {
+            header.css('top', '59px') ;
+        } else {
+            header.css('top', '541px');
+        }
+    });
 });
+
+// Carousel
+ $(document).ready(function() {
+    $('.carousel').carousel({
+      interval: 3000
+    })
+  });
