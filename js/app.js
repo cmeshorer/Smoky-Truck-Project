629fc7714bbbc8320b628c1ec3efdc54fc9176a4
Anne-Laure Bienaimé <annelaure.bienaime@gmail.com>
2017-10-19 23:12:25 +0200

    Riding logo first attempt

--- js/app.js
+++ js/app.js
@@ -11,12 +11,16 @@ $(window).scroll(function(){
     $('#navbar').addClass('scrolled-nav');
     $('#menu li a').css('color', '#b3b3b3');
     $('#menu .navactive').css('color', 'white');
-    $('nav img').attr('src', 'img/smoky_logo_scrolled.svg');
+    $("#logo1").css("display", "none");
+    $("#logo2").css("display", "none");
+    $("#scrolled_logo").css("display", "block");
   } else if (scrollTop < 200) {
     $('#navbar').removeClass('scrolled-nav');
     $('#menu li a').css('color', '#012051');
+    $("#logo1").css("display", "block");
+    $("#logo2").css("display", "block");
+    $("#scrolled_logo").css("display", "none");
     $('#menu .navactive').css('color', '#b3b3b3');
-    $('nav img').attr('src', 'img/smoky_logo.svg');
   } 
 }); 
 
