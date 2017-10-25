e3b4faf3b1efbd1e857e6e779312c92b4da17808
cadmoral <cams_00@hotmail.com>
2017-10-25 23:41:44 +0200

    admin pugs and routes

---
+++ routes/admin.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var router = express.Router();
+//const mysql = require('mysql');
+
+/* 
+
+--->GET ADMIN<----
+
+const connection = mysql.createConnection({
+ 	host     : 'localhost',
+	user     : 'root',
+	password : 'root',
+	database : 'groupe1'
+
+});
+
+*/
+
+
+/* GET ADMIN */
+
+router.get('/', function(req, res, next) {
+	// Page de connexion
+  res.render('login');
+});
+
+/*
+
+
+router.post('/', function(req, res, next) {
+	// Ici on gère les informations de l'utilisateur
+	// tester si l'utilisateur existe en BDD 
+	// Si l'utilisateur on ouvre sa session 
+
+	console.log(req.body);
+	let login = req.body.login ;
+	let password= req.body.password;
+	connection.query('SELECT * FROM users  WHERE name = "' + login + '" AND password = "' + password + '";', function (error, results, fields) {
+	  if (error) throw error;
+
+	  // console.log('The solution is: ', results[0].solution);
+
+	  if (results.length === 0) {
+	  	res.send("Cet utilisateur n'existe pas");
+	  } else {
+	  	req.session.connected = true;
+	  	res.redirect('/admin/index');
+	  }
+	});
+  	
+});
+
+router.get('/admin', function(req, res, next) {
+
+	// Hello session !
+	// Si pas connecté alors redirection
+
+	if (req.session.connected){
+		res.render('admin/index');
+	} else {
+		res.redirect('/');
+	}
+});
+*/
+
+
+router.get('/index', function(req, res, next) {
+  res.render('index-admin');
+});
+
+router.get('/editor', function(req, res, next) {
+  res.render('editor');
+});
+
+
+router.get('/menu', function(req, res, next) {
+  res.render('menu-admin');
+});
+
+router.get('/lieu-hor', function(req, res, next) {
+  res.render('lieu-hor-admin');
+});
+
+
+
+module.exports = router;
