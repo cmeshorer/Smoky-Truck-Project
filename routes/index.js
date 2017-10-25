2322accbc6893aa771dae32f8836bd5e04849714
palimpZest <polo.vaca365@gmail.com>
2017-10-25 14:00:35 +0200

    feat: Migrate index to pug and create error page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+
+router.get('/mentions', function(req, res, next) {
+  res.render('mentions');
+});
+
+router.get('/error', function(req, res, next) {
+  res.render('error');
 });
 
 module.exports = router;
