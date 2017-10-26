006570637cb096dbb41d9611eb3f0a0f2b7a13bb
cadmoral <cams_00@hotmail.com>
2017-10-26 12:09:31 +0200

    route admin

--- routes/admin.js
+++ routes/admin.js
@@ -2,68 +2,44 @@ var express = require('express');
 var router = express.Router();
 //const mysql = require('mysql');
 
-/* 
+/* Database connexion */
+/* const connection = mysql.createConnection({
+host : 'localhost',
+user : 'root',
+password : 'root',
+database : 'groupe1'
+}); */
 
---->GET ADMIN<----
-
-const connection = mysql.createConnection({
- 	host     : 'localhost',
-	user     : 'root',
-	password : 'root',
-	database : 'groupe1'
-
-});
-
-*/
-
-
-/* GET ADMIN */
-
+/* GET admin page login */
 router.get('/', function(req, res, next) {
-	// Page de connexion
-  res.render('login');
+res.render('login');
 });
 
-/*
+/* Users verification */ // On vérifie si l'utilisateur existe en BDD.
+/* router.post('/', function(req, res, next) {
+let login = req.body.login ;
+let password= req.body.password;
+connection.query(`select * from users where login= "${login}" and password="${password}"`, function (error, results, fields) {
+if (error) throw error; 
+if (results.length === 0) {
+res.send("Cet utilisateur n'existe pas"); // S'il pas résultat dans la BDD l'utilisateur n'existe pas et on lui envoie un message pour l'informer.
+} else {
+req.session.connect = true;
+res.redirect('/admin/index'); // Si l'utilisateur existe on ouvre la session et on le redirige sur l'espace admin.
+}
+});	
+}); */
 
+/* User redirection */
+/*router.get('/admin/index', function(req, res, next) {
+if (req.session.connected){
+res.render('admin/index'); // Si l'utilisateur est connecté il accède à l'espace d'administration sur l'index.
+} else {
+res.redirect('/'); // Sinon il retourne au login
+}
+}); */
 
-router.post('/', function(req, res, next) {
-	// Ici on gère les informations de l'utilisateur
-	// tester si l'utilisateur existe en BDD 
-	// Si l'utilisateur on ouvre sa session 
-
-	console.log(req.body);
-	let login = req.body.login ;
-	let password= req.body.password;
-	connection.query('SELECT * FROM users  WHERE name = "' + login + '" AND password = "' + password + '";', function (error, results, fields) {
-	  if (error) throw error;
-
-	  // console.log('The solution is: ', results[0].solution);
-
-	  if (results.length === 0) {
-	  	res.send("Cet utilisateur n'existe pas");
-	  } else {
-	  	req.session.connected = true;
-	  	res.redirect('/admin/index');
-	  }
-	});
-  	
-});
-
-router.get('/admin', function(req, res, next) {
-
-	// Hello session !
-	// Si pas connecté alors redirection
-
-	if (req.session.connected){
-		res.render('admin/index');
-	} else {
-		res.redirect('/');
-	}
-});
-*/
-
-
+/* Admin routes */
 router.get('/index', function(req, res, next) {
   res.render('admin-index');
 });
