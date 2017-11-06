5ebcc1d72729dee871720e8906ee5f680baf9d39
Anne-Laure Bienaimé <annelaure.bienaime@gmail.com>
2017-11-06 18:24:55 +0100

    New database

--- smoky_truck.sql
+++ smoky_truck.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.20, for macos10.12 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
 --
 -- Host: localhost    Database: smoky_truck
 -- ------------------------------------------------------
--- Server version	5.7.20
+-- Server version	5.7.20-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -22,30 +22,6 @@
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `smoky_truck` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 USE `smoky_truck`;
-
---
--- Table structure for table `category`
---
-
-DROP TABLE IF EXISTS `category`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `category` (
-`idcategory` int(11) NOT NULL AUTO_INCREMENT,
-`category` varchar(10) DEFAULT NULL,
-PRIMARY KEY (`idcategory`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `category`
---
-
-LOCK TABLES `category` WRITE;
-/*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'entree'),(2,'plat'),(3,'dessert');
-/*!40000 ALTER TABLE `category` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `actus`
@@ -75,6 +51,30 @@ INSERT INTO `actus` VALUES (2,'news-2.jpg','Titre 2',NULL,'bla bla bla bla bla b
 UNLOCK TABLES;
 
 --
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `idcategory` int(11) NOT NULL AUTO_INCREMENT,
+  `category` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`idcategory`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'entree'),(2,'plat'),(3,'dessert');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `menu`
 --
 
@@ -93,7 +93,7 @@ CREATE TABLE `menu` (
   PRIMARY KEY (`idmenu`),
   KEY `category_idx` (`category`),
   CONSTRAINT `category` FOREIGN KEY (`category`) REFERENCES `category` (`idcategory`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,7 +102,7 @@ CREATE TABLE `menu` (
 
 LOCK TABLES `menu` WRITE;
 /*!40000 ALTER TABLE `menu` DISABLE KEYS */;
-INSERT INTO `menu` VALUES (1,'pizza','rica pizza',5,'deux','image.png','icon.png',2);
+INSERT INTO `menu` VALUES (4,'Test entrée 2','<p>bla bla bla</p>',2,'2','entrée2.jpg','picto_vide.png',1),(5,'Entrée 3','<p>bla bla bla</p>',2,'2','entrée3.jpg','picto_vege.png',1),(7,'Pizza 1','<p>bla bla bla</p>',10,'1','pizza1.jpg','picto_gluten.png',2),(9,'Brushettas','<p>brushettes muy bonitas</p>',2,'2','entrée4.jpg','picto_gluten.png',1),(10,'Entrée 4','<p>dzzd</p>',2,'2','entrée1.jpg','picto_gluten.png',1),(11,'Entrée 5','<p>entr&eacute;e 5</p>',5,'2','news-3.jpg','picto_lactose.png',1);
 /*!40000 ALTER TABLE `menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -126,7 +126,7 @@ CREATE TABLE `places` (
 
 LOCK TABLES `places` WRITE;
 /*!40000 ALTER TABLE `places` DISABLE KEYS */;
-INSERT INTO `places` VALUES (1,'Smoky Rive Gauche'),(2,'Smoky 104'),(3,'Smoky Rive Droite'),(4,'Smoky Rive Gauche'),(5,'Smoky 104'),(6,'Smoky 104'),(7,'Smoky Rive Gauche'),(8,'Smoky Rive Droite'),(9,'Smoky Rive Droite'),(10,'Smoky Rive Gauche');
+INSERT INTO `places` VALUES (1,'Smoky 104'),(2,'Smoky 104'),(3,'Smoky Rive Droite'),(4,'Smoky Rive Gauche'),(5,'Smoky 104'),(6,'Smoky 104'),(7,'Smoky Rive Gauche'),(8,'Smoky Rive Droite'),(9,'Smoky Rive Droite'),(10,'Smoky Rive Gauche');
 /*!40000 ALTER TABLE `places` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -164,4 +164,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-03 20:26:47
+-- Dump completed on 2017-11-06 18:23:45
